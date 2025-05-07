4f022fd1dcc71b568e737122410029afe0a3a705
vvduth <ducthai060501@gmail.com>
2025-05-07 09:22:35 +0300

    feat: implement PDF summary storage and error handling with database integration

--- actions/upload-actions.ts
+++ actions/upload-actions.ts
@@ -1,9 +1,18 @@
-
-"use server"
+"use server";
 
 import { summarizeTextFromGemini } from "@/lib/geminiai";
 import { fetchAndExtractPdftext } from "@/lib/langChain";
 import { summarizeTextFromOpenAI } from "@/lib/openai";
+import { auth } from "@clerk/nextjs/server";
+import { getDbConnection } from "./db-actions";
+
+interface PDFSummaryType {
+  userid: string;
+  fileUrl: string;
+  summary: string;
+  title?: string;
+  fileName?: string;
+}
 
 export async function generatePdfSummary(
   uploadresponse: [
@@ -39,16 +48,19 @@ export async function generatePdfSummary(
     let summary;
     try {
       summary = await summarizeTextFromOpenAI(pdfText);
-      console.log( summary);
+      console.log(summary);
     } catch (error) {
       console.error("Error occurred while summarizing text: ", error);
-      // call gemini 
-      if (error instanceof Error && error.message === 'RATE_LIMIT_EXCEEDED') {
+      // call gemini
+      if (error instanceof Error && error.message === "RATE_LIMIT_EXCEEDED") {
         try {
-          summary  = await summarizeTextFromGemini(pdfText);
+          summary = await summarizeTextFromGemini(pdfText);
           console.log("Gemini summary", summary);
         } catch (geminiError) {
-          console.error("Gemini API failer after openai quota exceeded", geminiError);
+          console.error(
+            "Gemini API failer after openai quota exceeded",
+            geminiError
+          );
           throw new Error("Gemini API failed after OpenAI quota exceeded");
         }
       }
@@ -63,14 +75,89 @@ export async function generatePdfSummary(
     }
 
     return {
-        success: true,
-        message: "Summary generated successfully",
-        data: {summary},
-    }
+      success: true,
+      message: "Summary generated successfully",
+      data: { summary },
+    };
   } catch (error) {
     return {
       success: false,
       message: "Error occurred while generating summary",
+      data: null,
+    };
+  }
+}
+
+async function savePdfSummaryToDB({
+  userid,
+  fileUrl,
+  summary,
+  title,
+  fileName,
+}: PDFSummaryType) {
+  {
+    try {
+      const sql = await getDbConnection();
+      await sql`INSERT INTO pdf_summaries (user_id, original_file_url, summary_text, title, file_name)
+  VALUES (
+      ${userid},
+      ${fileUrl},
+      ${summary},
+      ${title},
+      ${fileName},
+  );`;
+    } catch (error) {
+      console.error("Error saving pdf sumamry", error);
+      throw error;
+    }
+  }
+}
+export async function storePDFSummary({
+  fileUrl,
+  summary,
+  title,
+  fileName,
+}: PDFSummaryType) {
+  let savePdfSummary:any;
+  try {
+    // get clerk user id from server side
+    const { userId } = await auth();
+    if (!userId) {
+      return {
+        success: false,
+        message: "No user id found",
+        data: null,
+      };
+    }
+
+    savePdfSummary = await savePdfSummaryToDB({
+      userid: userId,
+      fileUrl,
+      summary,
+      title,
+      fileName,
+    });
+
+    if (!savePdfSummary) {
+      return {
+        success: false,
+        message: "No summary saved, please try again",
+        data: null,
+      };
+    }
+
+    return {
+      success: true,
+      message: "Summary saved successfully",
+      
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message:
+        error instanceof Error
+          ? error.message
+          : "Error occurred while storing summary",
       data: null,
     };
   }
